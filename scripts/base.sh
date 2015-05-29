7c9f3267379d63ffd43c45b916c639cd95f237ec
Markus Frosch <markus@lazyfrosch.de>
2015-05-29 13:45:32 +0200

    Simplify grub settings

--- scripts/base.sh
+++ scripts/base.sh
@@ -15,15 +15,5 @@ echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
 echo 'UseDNS no' >> /etc/ssh/sshd_config
 
 # Remove 5s grub timeout to speed up booting
-cat <<EOF > /etc/default/grub
-# If you change this file, run 'update-grub' afterwards to update
-# /boot/grub/grub.cfg.
-
-GRUB_DEFAULT=0
-GRUB_TIMEOUT=0
-GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
-GRUB_CMDLINE_LINUX_DEFAULT="quiet"
-GRUB_CMDLINE_LINUX="debian-installer=en_US"
-EOF
-
+sed -i 's/^\(GRUB_TIMEOUT\).*/#\0\n\1=0/' /etc/default/grub
 update-grub
